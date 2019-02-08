8ea4690b5f4ce43ff38327f6b37536d4f654059d
hbishop1 <hughbishop@live.co.uk>
2019-02-08 17:51:33 +0000

    added run.sh file

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH -c 1
+
+#SBATCH --gres=gpu:1
+#SBATCH --qos="debug"
+#SBATCH -p "ug-gpu-small"
+#SBATCH -t 02:00:00
+
+source /etc/profile
+module load cuda/8.0
+
+stdbuf -oL python3 ncc-project/transfer_resnet_cnn.py
--- transfer_resnet_cnn.py
+++ transfer_resnet_cnn.py
@@ -8,45 +8,7 @@ import time
 import torchvision
 from torchvision import transforms, datasets, models
 
-
-# hyper-parameters
-learning_rate = 0.0001
-training_iterations = 25
-
-simple_transforms = transforms.Compose([transforms.Resize((224,224)),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225])])
-
-train = datasets.ImageFolder('./dataset_fine-grained/train/',simple_transforms)
-valid = datasets.ImageFolder('./dataset_fine-grained/valid/',simple_transforms)
-
-def imshow(inp):
-    inp = inp.numpy().transpose((1,2,0))
-    mean = np.array([0.485,0.456,0.406])
-    std = np.array([0.229,0.224,0.225])
-    inp = std * inp + mean
-    inp = np.clip(inp,0,1)
-    plt.imshow(inp)
-    plt.show()
-
-train_data_gen = torch.utils.data.DataLoader(train,batch_size = 8,num_workers=1)
-valid_data_gen = torch.utils.data.DataLoader(valid,batch_size = 8,num_workers=1)
-dataloaders = [train_data_gen,valid_data_gen]
-
-model_ft = models.resnet18(pretrained=True)
-num_ftrs = model_ft.fc.in_features
-model_ft.fc = nn.Linear(num_ftrs,len(train.classes))
-
-if torch.cuda.is_available():
-    model_ft = model_ft.cuda()
-
-criterion = nn.CrossEntropyLoss()
-optimizer_ft = optim.SGD(model_ft.parameters(),lr = learning_rate,momentum = 0.9)
-exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size = 7, gamma = 0.1)
-
-
-
-def train_model(model, criterion, optimizer, scheduler, num_epochs=training_iterations):
+def train_model(model, criterion, optimizer, scheduler, num_epochs):
     since = time.time()
 
     best_model_wts = model.state_dict()
@@ -68,8 +30,6 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=training_iter
 
             for it,data in enumerate(dataloaders[i]):
 
-                print(it)
-
                 inputs,labels = data
 
                 if torch.cuda.is_available():
@@ -90,6 +50,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=training_iter
 
                 running_loss += loss.data
                 running_corrects += torch.sum(preds == labels.data)
+                
 
 
             epoch_loss = running_loss / len(dataloaders[i].dataset)
@@ -111,5 +72,43 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=training_iter
     return model
 
 
-train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, training_iterations)
+if __name__ == '__main__':
+
+    learning_rate = 0.00001
+    training_iterations = 50
+
+    simple_transforms = transforms.Compose([transforms.Resize((224,224)),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225])])
+
+    train = datasets.ImageFolder('./dataset_fine-grained/train/',simple_transforms)
+    valid = datasets.ImageFolder('./dataset_fine-grained/valid/',simple_transforms)
+
+
+    train_data_gen = torch.utils.data.DataLoader(train,batch_size = 8,num_workers=1)
+    valid_data_gen = torch.utils.data.DataLoader(valid,batch_size = 8,num_workers=1)
+    dataloaders = [train_data_gen,valid_data_gen]
+
+    model_ft = models.resnet18(pretrained=True)
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs,len(train.classes))
+
+    if torch.cuda.is_available():
+        model_ft = model_ft.cuda()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer_ft = optim.SGD(model_ft.parameters(),lr = learning_rate,momentum = 0.9)
+    exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size = 7, gamma = 0.1)
+
+    train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, training_iterations)
+
+def imshow(inp):
+    inp = inp.numpy().transpose((1,2,0))
+    mean = np.array([0.485,0.456,0.406])
+    std = np.array([0.229,0.224,0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp,0,1)
+    plt.imshow(inp)
+    plt.show()
+
 
