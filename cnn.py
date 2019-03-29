49ac809096a96643b3491ed59265e30296a7887d
hbishop1 <hughbishop@live.co.uk>
2019-03-29 17:31:15 +0000

    added l2 reg

--- cnn.py
+++ cnn.py
@@ -197,7 +197,7 @@ if __name__ == '__main__':
 
     criterion = nn.CrossEntropyLoss()
 
-    optimizer_ft = optim.Adam(model_ft.parameters(),lr = learning_rate,weight_decay=0.01)
+    optimizer_ft = optim.Adam(model_ft.parameters(),lr = learning_rate,weight_decay=0.03)
 
     train_model(model_ft, criterion, optimizer_ft, training_iterations)
 
