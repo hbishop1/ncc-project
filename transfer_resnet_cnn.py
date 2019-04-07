23b76f6dccac9b4fca1abed9d801b38076b647cf
hbishop1 <hughbishop@live.co.uk>
2019-04-07 14:20:19 +0100

    higher lr

--- transfer_resnet_cnn.py
+++ transfer_resnet_cnn.py
@@ -73,13 +73,13 @@ def train_model(model, criterion, optimizer, num_epochs=25):
 
     best_acc = 0.0
 
-    open('results_transfer.txt','w')
+    open('results_transfer1.txt','w')
 
     for epoch in range(1,num_epochs+1):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
 
-        with open('results_transfer.txt','a') as results:
+        with open('results_transfer1.txt','a') as results:
             results.write('Epoch {}/{} \n'.format(epoch,num_epochs))
         
 
@@ -124,7 +124,7 @@ def train_model(model, criterion, optimizer, num_epochs=25):
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
 
-            with open('results_transfer.txt','a') as results:
+            with open('results_transfer1.txt','a') as results:
                 results.write('{} Loss: {:.4f} Acc: {:.4f} \n'.format(phase, epoch_loss, epoch_acc))
 
         print()
@@ -141,7 +141,7 @@ if __name__ == '__main__':
 
     # lr = 5e-6 is best for cross entropy
 
-    learning_rate = 1e-6
+    learning_rate = 1e-5
     training_iterations = 500
 
     data_transforms = {
