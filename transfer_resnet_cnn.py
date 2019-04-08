2ed6dfcda934c9de1bc38d691729840628624264
hbishop1 <hughbishop@live.co.uk>
2019-04-08 12:40:05 +0100

    testing loss function change after 100 epochs

--- transfer_resnet_cnn.py
+++ transfer_resnet_cnn.py
@@ -176,7 +176,7 @@ if __name__ == '__main__':
 
     # lr = 5e-6 is best for cross entropy
 
-    learning_rate = 5e-5
+    learning_rate = 5e-6
     training_iterations = 500
 
     data_transforms = {
