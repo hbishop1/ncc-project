bd8a105802697b7bf268ff02e3e45e8daf9cb47e
hbishop1 <hughbishop@live.co.uk>
2019-04-08 19:11:15 +0100

    testing loss function change after 100 epochs

--- transfer_resnet_cnn.py
+++ transfer_resnet_cnn.py
@@ -95,22 +95,22 @@ def train_model(model, criterion, optimizer, num_epochs=25):
 
     best_acc = 0.0
 
-    open('results_transfer3.txt','w')
+    open('results_transfer.txt','w')
 
     for epoch in range(1,num_epochs+1):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
 
-        if epoch == 201:
-            with open('results_transfer3.txt','a') as results:
+        if epoch % 1 == 0:
+            with open('results_transfer.txt','a') as results:
                 results.write('Switching to heirachical graph \n')
             criterion.heirachy_graph()
-        # elif epoch % 10 == 5:
-        #     with open('results_transfer.txt','a') as results:
-        #         results.write('Switching to heirachical graph \n')
-        #     criterion.heirachy_graph()
+        else epoch % 10 == 5:
+             with open('results_transfer.txt','a') as results:
+                results.write('Switching to heirachical graph \n')
+            criterion.heirachy_graph()
 
-        with open('results_transfer3.txt','a') as results:
+        with open('results_transfer.txt','a') as results:
             results.write('Epoch {}/{} \n'.format(epoch,num_epochs))
         
 
@@ -159,7 +159,7 @@ def train_model(model, criterion, optimizer, num_epochs=25):
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
 
-            with open('results_transfer3.txt','a') as results:
+            with open('results_transfer.txt','a') as results:
                 results.write('{} Loss: {:.4f} Acc: {:.4f} \n'.format(phase, epoch_loss, epoch_acc))
 
         print()
@@ -176,7 +176,7 @@ if __name__ == '__main__':
 
     # lr = 5e-6 is best for cross entropy
 
-    learning_rate = 5e-5
+    learning_rate = 5e-6
     training_iterations = 200
 
     data_transforms = {
