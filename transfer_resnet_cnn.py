5b42432222e9f2f405e86392d8849afdbe28eafc
hbishop1 <hughbishop@live.co.uk>
2019-04-08 15:58:20 +0100

    testing loss function change after 100 epochs

--- transfer_resnet_cnn.py
+++ transfer_resnet_cnn.py
@@ -95,14 +95,14 @@ def train_model(model, criterion, optimizer, num_epochs=25):
 
     best_acc = 0.0
 
-    open('results_transfer.txt','w')
+    open('results_transfer2.txt','w')
 
     for epoch in range(1,num_epochs+1):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
 
         if epoch == 201:
-            with open('results_transfer.txt','a') as results:
+            with open('results_transfer2.txt','a') as results:
                 results.write('Switching to heirachical graph \n')
             criterion.heirachy_graph()
         # elif epoch % 10 == 5:
@@ -110,7 +110,7 @@ def train_model(model, criterion, optimizer, num_epochs=25):
         #         results.write('Switching to heirachical graph \n')
         #     criterion.heirachy_graph()
 
-        with open('results_transfer.txt','a') as results:
+        with open('results_transfer2.txt','a') as results:
             results.write('Epoch {}/{} \n'.format(epoch,num_epochs))
         
 
@@ -159,7 +159,7 @@ def train_model(model, criterion, optimizer, num_epochs=25):
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
 
-            with open('results_transfer.txt','a') as results:
+            with open('results_transfer2.txt','a') as results:
                 results.write('{} Loss: {:.4f} Acc: {:.4f} \n'.format(phase, epoch_loss, epoch_acc))
 
         print()
