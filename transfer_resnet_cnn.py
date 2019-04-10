d2587a38f6f5ba528662fbe00180940d78b795c5
hbishop1 <hughbishop@live.co.uk>
2019-04-10 12:54:01 +0100

    made tree all of depth 4

--- transfer_resnet_cnn.py
+++ transfer_resnet_cnn.py
@@ -102,17 +102,17 @@ def train_model(model, criterion, optimizer, num_epochs=25):
         print('Epoch {}/{}'.format(epoch, num_epochs))
         print('-' * 10)
 
-        if epoch % 20 == 10:
-            with open('results_transfer.txt','a') as results:
-                results.write('Switching to heirachical graph \n')
-            criterion.heirachy_graph()
-        elif epoch % 20 == 0 and epoch != 0:
-            with open('results_transfer.txt','a') as results:
-                results.write('Switching to flat graph \n')
-            criterion.flat_graph()
+        # if epoch % 20 == 10:
+        #     with open('results_transfer.txt','a') as results:
+        #         results.write('Switching to heirachical graph \n')
+        #     criterion.heirachy_graph()
+        # elif epoch % 20 == 0 and epoch != 0:
+        #     with open('results_transfer.txt','a') as results:
+        #         results.write('Switching to flat graph \n')
+        #     criterion.flat_graph()
 
-        with open('results_transfer.txt','a') as results:
-            results.write('Epoch {}/{} \n'.format(epoch,num_epochs))
+        # with open('results_transfer.txt','a') as results:
+        #     results.write('Epoch {}/{} \n'.format(epoch,num_epochs))
         
 
         # Each epoch has a training and validation phase
@@ -219,7 +219,7 @@ if __name__ == '__main__':
 
     criterion = Heirachical_Loss()
 
-    criterion.flat_graph()
+    criterion.heirachy_graph()
 
     optimizer_ft = optim.Adam(model_ft.parameters(),lr = learning_rate,weight_decay=0.005)
 
