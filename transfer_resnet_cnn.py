538b68b4c057016edcfa86ae13fc6084933a18a1
hbishop1 <hughbishop@live.co.uk>
2019-04-10 18:35:44 +0100

    made tree all of depth 4

--- transfer_resnet_cnn.py
+++ transfer_resnet_cnn.py
@@ -103,7 +103,7 @@ def train_model(model, criterion, optimizer, num_epochs=25):
 
     best_acc = 0.0
 
-    open('results_transfer.txt','w')
+    open('results_transfer1.txt','w')
 
     for epoch in range(1,num_epochs+1):
         print('Epoch {}/{}'.format(epoch, num_epochs))
@@ -118,7 +118,7 @@ def train_model(model, criterion, optimizer, num_epochs=25):
         #         results.write('Switching to flat graph \n')
         #     criterion.flat_graph()
 
-        with open('results_transfer.txt','a') as results:
+        with open('results_transfer1.txt','a') as results:
             results.write('Epoch {}/{} \n'.format(epoch,num_epochs))
         
 
@@ -169,7 +169,7 @@ def train_model(model, criterion, optimizer, num_epochs=25):
             print('{} Loss: {:.4f} Acc: {:.4f} Dist: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc, epoch_dist))
 
-            with open('results_transfer.txt','a') as results:
+            with open('results_transfer1.txt','a') as results:
                 results.write('{} Loss: {:.4f} Acc: {:.4f} Dist: {:.4f} \n'.format(
                     phase, epoch_loss, epoch_acc, epoch_dist))
 
@@ -231,7 +231,7 @@ if __name__ == '__main__':
 
     criterion.flat_graph()
 
-    optimizer_ft = optim.Adam(model_ft.parameters(),lr = learning_rate,weight_decay=0.005)
+    optimizer_ft = optim.Adam(model_ft.parameters(),lr = learning_rate,weight_decay=0.1)
 
     train_model(model_ft, criterion, optimizer_ft, training_iterations)
 
