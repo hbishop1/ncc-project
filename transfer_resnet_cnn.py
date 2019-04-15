b7682a0a437d707bfbfef20c950cee192796c056
hbishop1 <hughbishop@live.co.uk>
2019-04-15 12:41:09 +0100

    testing untrained alexnet

--- transfer_resnet_cnn.py
+++ transfer_resnet_cnn.py
@@ -103,7 +103,7 @@ def train_model(model, criterion, optimizer, num_epochs=25):
 
     best_acc = 0.0
 
-    open('results_transfer1.txt','w')
+    open('results_transfer.txt','w')
 
     for epoch in range(1,num_epochs+1):
         print('Epoch {}/{}'.format(epoch, num_epochs))
@@ -118,7 +118,7 @@ def train_model(model, criterion, optimizer, num_epochs=25):
         #         results.write('Switching to heirachical graph \n')
         #     criterion.heirachy_graph()
 
-        with open('results_transfer1.txt','a') as results:
+        with open('results_transfer.txt','a') as results:
             results.write('Epoch {}/{} \n'.format(epoch,num_epochs))
         
 
@@ -169,7 +169,7 @@ def train_model(model, criterion, optimizer, num_epochs=25):
             print('{} Loss: {:.4f} Acc: {:.4f} Dist: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc, epoch_dist))
 
-            with open('results_transfer1.txt','a') as results:
+            with open('results_transfer.txt','a') as results:
                 results.write('{} Loss: {:.4f} Acc: {:.4f} Dist: {:.4f} \n'.format(
                     phase, epoch_loss, epoch_acc, epoch_dist))
 
@@ -187,7 +187,7 @@ if __name__ == '__main__':
 
     # lr = 5e-6 is best for cross entropy
 
-    learning_rate = 5e-6
+    learning_rate = 5e-5
     training_iterations = 200
 
     data_transforms = {
