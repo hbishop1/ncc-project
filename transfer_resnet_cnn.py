d7dd476185cb0539ffffa4ce873719a12f590340
hbishop1 <hughbishop@live.co.uk>
2019-04-25 15:47:16 +0100

    testing vgg with hl

--- transfer_resnet_cnn.py
+++ transfer_resnet_cnn.py
@@ -122,14 +122,14 @@ def train_model(model, criterion, optimizer, num_epochs=25, outfile='results'):
         print('Epoch {}/{}'.format(epoch, num_epochs))
         print('-' * 10)
 
-        # if epoch % 10 == 0 and epoch != 0:
-        #     with open(outfile + '.txt','a') as results:
-        #         results.write('Switching to flat graph \n')
-        #     criterion.flat_graph()
-        # elif epoch % 10 == 1:
-        #     with open(outfile + '.txt','a') as results:
-        #         results.write('Switching to hierachical graph \n')
-        #     criterion.hierachy_graph()
+        if epoch % 10 == 0 and epoch != 0:
+            with open(outfile + '.txt','a') as results:
+                results.write('Switching to flat graph \n')
+            criterion.flat_graph()
+        elif epoch % 10 == 1:
+            with open(outfile + '.txt','a') as results:
+                results.write('Switching to hierachical graph \n')
+            criterion.hierachy_graph()
 
         with open(outfile + '.txt','a') as results:
             results.write('Epoch {}/{} \n'.format(epoch,num_epochs))
@@ -209,7 +209,7 @@ if __name__ == '__main__':
     learning_rate = 1e-5
     training_iterations = 200
 
-    out = 'results_hl_reversed'
+    out = 'results_hl_reversed_alternating'
 
     data_transforms = {
     'train': transforms.Compose([
