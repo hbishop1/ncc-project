366daf1b3d8bb2fbaf75ad2809ffbb72af0ff897
hbishop1 <hughbishop@live.co.uk>
2019-04-26 18:49:51 +0100

    testing hl

--- hierachical_loss.py
+++ hierachical_loss.py
@@ -216,7 +216,7 @@ if __name__ == '__main__':
     learning_rate = 1e-5
     training_iterations = 200
 
-    out = 'test_hl'
+    out = 'test_ce'
 
     data_transforms = {
     'train': transforms.Compose([
@@ -276,7 +276,7 @@ if __name__ == '__main__':
     
     model = model.to(device)
 
-    criterion = Heirachical_Loss(hierachical=True, reversed_weights=True)
+    criterion = Heirachical_Loss(hierachical=False, reversed_weights=True)
 
     optimizer = optim.Adam(model.parameters(),lr = learning_rate,weight_decay=0.01)
 
