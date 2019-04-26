ebd6587d30e2d3c418295a92acc27b3151c2b24a
hbishop1 <hughbishop@live.co.uk>
2019-04-26 18:47:03 +0100

    testing new hl

--- transfer_resnet_cnn.py
+++ transfer_resnet_cnn.py
@@ -122,14 +122,14 @@ def train_model(model, criterion, optimizer, num_epochs=25, outfile='results'):
         print('Epoch {}/{}'.format(epoch, num_epochs))
         print('-' * 10)
 
-        # if epoch % 10 == 0 and epoch != 0:
-        #     with open(outfile + '.txt','a') as results:
-        #         results.write('Switching to flat graph \n')
-        #     criterion.hierachy_graph()
-        # elif epoch % 10 == 1:
-        #     with open(outfile + '.txt','a') as results:
-        #         results.write('Switching to hierachical graph \n')
-        #     criterion.flat_graph()
+        if epoch % 10 == 0 and epoch != 0:
+            with open(outfile + '.txt','a') as results:
+                results.write('Switching to flat graph \n')
+            criterion.hierachy_graph()
+        elif epoch % 10 == 1:
+            with open(outfile + '.txt','a') as results:
+                results.write('Switching to hierachical graph \n')
+            criterion.flat_graph()
 
         with open(outfile + '.txt','a') as results:
             results.write('Epoch {}/{} \n'.format(epoch,num_epochs))
@@ -269,7 +269,7 @@ if __name__ == '__main__':
     
     model = model.to(device)
 
-    criterion = nn.CrossEntropyLoss()#Heirachical_Loss(hierachical=True, reversed_weights=True)
+    criterion = nn.CrossEntropyLossHeirachical_Loss(hierachical=True, reversed_weights=True)
 
     optimizer = optim.Adam(model.parameters(),lr = learning_rate,weight_decay=0.01)
 
