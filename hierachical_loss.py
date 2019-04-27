a21c28d9e83b474bf13bb85bbd87d01f858d2d45
hbishop1 <hughbishop@live.co.uk>
2019-04-27 16:40:10 +0100

    alternating reversed

--- hierachical_loss.py
+++ hierachical_loss.py
@@ -233,7 +233,7 @@ if __name__ == '__main__':
     learning_rate = 1e-5
     training_iterations = 200
 
-    out = 'test_hl_reversed'
+    out = 'test_hl'
 
     data_transforms = {
     'train': transforms.Compose([
@@ -293,7 +293,7 @@ if __name__ == '__main__':
     
     model = model.to(device)
 
-    criterion = Heirachical_Loss(hierachical=True, reversed_weights=True)
+    criterion = Heirachical_Loss(hierachical=True, reversed_weights=False)
 
     optimizer = optim.Adam(model.parameters(),lr = learning_rate,weight_decay=0.01)
 
