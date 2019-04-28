7bfa09839717be2764fb2b7a7d66f0cbcb440696
hbishop1 <hughbishop@live.co.uk>
2019-04-28 18:14:09 +0100

    testing alternating every 20

--- hierachical_loss.py
+++ hierachical_loss.py
@@ -232,7 +232,7 @@ if __name__ == '__main__':
     learning_rate = 1e-5
     training_iterations = 200
 
-    out = 'test_hl_alt_20'
+    out = 'test_rwhl_alt_20'
 
     data_transforms = {
     'train': transforms.Compose([
@@ -292,7 +292,7 @@ if __name__ == '__main__':
     
     model = model.to(device)
 
-    criterion = Heirachical_Loss(hierachical=True, reversed_weights=False)
+    criterion = Heirachical_Loss(hierachical=True, reversed_weights=True)
 
     optimizer = optim.Adam(model.parameters(),lr = learning_rate,weight_decay=0.01)
 
