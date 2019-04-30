585a7b33faba6b1be3c01747222bf7c45a613f9b
hbishop1 <hughbishop@live.co.uk>
2019-04-30 10:12:35 +0100

    hl alt

--- hierachical_loss.py
+++ hierachical_loss.py
@@ -144,11 +144,11 @@ def train_model(model, criterion, optimizer, num_epochs=25, outfile='results'):
         print('Epoch {}/{}'.format(epoch, num_epochs))
         print('-' * 10)
 
-        if epoch % 100 == 0 and epoch != 0:
+        if epoch % 2 == 0 and epoch != 0:
             with open(outfile + '.txt','a') as results:
                 results.write('Switching to flat graph \n')
             criterion.flat_graph()
-        elif epoch % 100 == 1:
+        elif epoch % 2 == 1:
             with open(outfile + '.txt','a') as results:
                 results.write('Switching to hierachical graph \n')
             criterion.hierachy_graph()
